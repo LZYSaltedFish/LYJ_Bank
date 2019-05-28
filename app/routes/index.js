6045a0bafdede455220a524e59cb17fa63aeb3e7
Lighting <995289312@qq.com>
2019-05-28 19:39:20 +0800

    更新readme

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ const todo = require('./todo')
 
 // 托管静态网页
 router.get('/', function (req, res) {
-  res.sendFile('../bank/html/index.html')
+  res.sendFile('../../bank/html/index.html')
 })
 
 router.use('/api', todo)
