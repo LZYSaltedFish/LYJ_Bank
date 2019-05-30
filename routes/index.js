5d4a9cc217bae26b752278dfa0a4b877d535928a
Superbia <995289312@qq.com>
2019-05-30 11:58:26 +0800

    修复登录接口

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
 const todo = require('./todo')
 const account = require('./account')
-const login = require('./login')
+const user = require('./user')
 const authenticate = require('./middlewares/authenticate')
 
 // 无需认证的接口
 router.get('/', (req, res) => { res.send('hello world') })
-router.use('/login', login)
+router.use('/', user)
 
 // 以下接口都需要认证
 router.use(authenticate)
